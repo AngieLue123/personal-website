d629691109eaa10ee5c7b59f09d8cd661e77eee8
AngieLue123 <angiel8686@gmail.com>
2025-08-04 23:48:55 -0700

    edits

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       {/* Header */}
       <header className="App-header">
-        <h1 className="name">Angelina Lue</h1>
+        <h1 className="name">Hi, I'm Angelina!</h1>
         <nav>
           {/* <a href="#writing">writing</a> */}
           {/* <a href="#experience">experience</a> */}
@@ -19,22 +19,22 @@ function App() {
         {/* About Section */}
         <section id="about" className="section">
           <p>
-            Hi! I'm Angelina! I am a product-obsessed builder based in the SF Bay Area. 
+            I'm a product-obsessed builder based in the SF Bay Area. 
             I recently graduated from UCLA with a double major in Computer Science and Economics and concentration in Value Investing.
           </p>
           <p> 
-            I most recently worked at Meta as an engineer building applied AI products. I have also been lucky to build with the incredible teams at <a href="https://mercury.com/">Mercury</a>, <a href="https://joincheckmate.com/">Checkmate</a>, <a href="https://papayapay.com/">Papaya</a>, and PayPal. 
+            I most recently worked at Meta as an engineer building applied AI products. I have also been lucky to build with the incredible teams at <a href="https://mercury.com/" target="_blank" rel="noopener noreferrer">Mercury</a>, <a href="https://joincheckmate.com/" target="_blank" rel="noopener noreferrer">Checkmate</a>, <a href="https://papayapay.com/" target="_blank" rel="noopener noreferrer">Papaya</a>, and PayPal. 
           </p>
           <p>
             While at UCLA, I caught the venture bug and invested part-time at General Catalyst (leading their Venture Fellowship), Mantis VC (the Chainsmokers' venture fund), and NEA. 
-            I also angel invest and have been very grateful to back <a href="https://aeolus.earth/">Aeolus</a>, <a href="https://basalt.space/">Basalt</a>, and <a href="https://www.tryminerva.ai/">Minerva AI</a>.
+            I also angel invest and have been very grateful to back <a href="https://aeolus.earth/" target="_blank" rel="noopener noreferrer">Aeolus</a>, <a href="https://basalt.space/" target="_blank" rel="noopener noreferrer">Basalt</a>, and <a href="https://www.tryminerva.ai/" target="_blank" rel="noopener noreferrer">Minerva AI</a>.
           </p>
           <p>
-            For fun, I love to snowboard, host dinners and poker nights, and update my <a href="https://beliapp.co/app/alue">Beli</a>  :) 
+            For fun, I love snowboarding, hosting dinner parties and poker nights, coffee, running to house music (Fisher, Sammy Virji, Mau P), and updating my <a href="https://beliapp.co/app/alue" target="_blank" rel="noopener noreferrer">Beli</a>  :) 
           </p>
           <p>
             I love meeting new people, please introduce yourself! Reach me at alue8 [at] ucla [dot] edu or 
-            message me on <a href="https://www.linkedin.com/in/angelina-lue/">LinkedIn</a>.
+            message me on <a href="https://www.linkedin.com/in/angelina-lue/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.
           </p>
         </section>
 
@@ -59,7 +59,7 @@ function App() {
       {/* Footer */}
       <footer className="App-footer">
         <p>
-          <a href="mailto:alue8@ucla.edu">Email</a>  •  <a href="https://www.linkedin.com/in/angelina-lue">LinkedIn</a>  •  <a href="https://x.com/angelina_lue?s=21">Twitter</a>  •   © 2025 Angelina Lue
+          <a href="mailto:alue8@ucla.edu">Email</a>  •  <a href="https://www.linkedin.com/in/angelina-lue" target="_blank" rel="noopener noreferrer">LinkedIn</a>  •  <a href="https://x.com/angelina_lue?s=21" target="_blank" rel="noopener noreferrer">Twitter</a>  •   © 2025 Angelina Lue
         </p>
       </footer>
     </div>
